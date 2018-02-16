811bba071504421b9ab13205e06db8f46d7186ba
jdvr1994 <33909059+jdvr1994@users.noreply.github.com>
2018-02-16 18:10:51 -0500

    Response Domiciliarios

--- controllers/domiciliario.js
+++ controllers/domiciliario.js
@@ -21,7 +21,7 @@ function getDomiciliarios(req, res){
     if(err)return res.status(500).send({message:`Error al realizar la petición ${err}`})
     if(domiciliarios.length == 0)return res.status(404).send({message:'No hay domiciliarios registrados'})
 
-    res.status(200).send({ domiciliarios })
+    res.status(200).send(domiciliarios)
   })
 }
 
